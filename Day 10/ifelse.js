d1c16a444975abadd85e58e9d18cbe8b604c1e81
Harishkumar79 <harishsen7976@gmail.com>
2024-07-04 22:56:38 +0530

    commit

---
+++ Day 10/ifelse.js
@@ -0,0 +1,71 @@
+let a = 60,
+    b = 70,
+    c = 80,
+    d = 90,
+    e = 100
+
+if(a>b) {
+    if(a>c) {
+        if(a>d) {
+            if(a>e) {
+                console.log("A is greater")
+            } else {
+                console.log("E is greater")
+            }
+        } else if(d>e){
+            console.log("D is greater")
+        } else {
+            console.log("E is greater")
+        }
+    } else if(c>d) {
+        console.log("C is greater")
+    } else if(d>e){
+        console.log("D is greater")
+    } else {
+        console.log("E is greater")
+    }
+} else if (b>a) {
+    if(b>c) {
+        if(b>d) {
+            if(b>e) {
+                console.log("B is greater")
+            } else {
+                console.log("E is greater")
+            }
+        } else if(d>e){
+            console.log("D is greater")
+        } else {
+            console.log("E is greater")
+        }
+    } else if(c>d) {
+        console.log("C is greater")
+    } else if(d>e){
+        console.log("D is greater")
+    } else {
+        console.log("E is greater")
+    }
+} else if(c>a){
+    if(c>b) {
+        if(c>d) {
+            if(c>e) {
+                console.log("C is greater")
+            } else {
+                console.log("E is greater")
+            }
+        } else if(d>e){
+            console.log("D is greater")
+        } else {
+            console.log("E is greater")
+        }
+    } else if(b>d) {
+        console.log("B is greater")
+    } else if(d>e){
+        console.log("D is greater")
+    } else {
+        console.log("E is greater")
+    }
+} else if(d>e){
+    console.log("D is greater")
+} else {
+    console.log("E is greater")
+}
