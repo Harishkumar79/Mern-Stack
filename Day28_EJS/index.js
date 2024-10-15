6b3614e8c8e3568436e4fcad6c1b3abe76d0d806
Harishkumar79 <harishsen7976@gmail.com>
2024-10-16 00:44:54 +0530

    commit

--- Day28_EJS/index.js
+++ Day28_EJS/index.js
@@ -14,6 +14,12 @@ app.set("views", path.join(__dirname , "/views"));
 app.get("/ejs",(req,res)=>{
     res.render('index.ejs');
 });
+app.get("/ejs/dice_game",(req,res)=>{
+    let diceValue = Math.floor(Math.random()*6)+1 
+    // res.render('dice_game.ejs' , {num : diceValue});  1.method
+    // res.render('dice_game.ejs' , {diceValue : diceValue});  2.method
+    res.render('dice_game.ejs' , {diceValue});  // 3.method
+});
 
 const port = 5000;
 app.listen(port ,()=>{
