6f2f5232f285f31255d17cfaa53d4ff1f2bba333
Harishkumar79 <harishsen7976@gmail.com>
2024-11-27 00:06:57 +0530

    commit

--- Day31/ReactJS/first/src/Components/Products/ProductList.jsx
+++ Day31/ReactJS/first/src/Components/Products/ProductList.jsx
@@ -18,8 +18,10 @@ function ProductList() {
                                     <div className='desc'>{data.description}</div>
                                     <div className='price'>${data.price}</div>
                                     <div className='view'></div>
-                                    <NavLink id='view-more' to='/'>View more</NavLink>
-                                    <NavLink to='/'><button id='card-btn' type='button'>Add To Cart</button></NavLink>
+                                    <span>
+                                        <NavLink id='view-more' to={`/product/${data.id}`}>View more</NavLink>
+                                        <NavLink to='/'><button id='card-btn' type='button'>Add To Cart</button></NavLink>
+                                    </span>
                                 </div>
                             </>
                         )
