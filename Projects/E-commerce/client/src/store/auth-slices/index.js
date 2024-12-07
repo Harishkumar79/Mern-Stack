6308d694b1f3dd38a64fdf0e05e558337e08bb60
Harishkumar79 <harishsen7976@gmail.com>
2024-12-08 00:00:26 +0530

    commit

---
+++ Projects/E-commerce/client/src/store/auth-slices/index.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    isAuthenticated : false,
+    isLoading : false,
+    user : null
+}
+
+const authSlice = createSlice({
+    name : "hk",
+    initialState,
+    reducers : {
+        setUser : (state , actions)=>{
+
+        }
+    }
+})
+
+export const {setUser} = authSlice.actions;
+export default authSlice.reducer;
